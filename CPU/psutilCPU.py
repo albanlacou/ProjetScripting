1974985cda7907bf74fecbefd8d1c37d88a0f5b0
alban <lacou.alban@gmail.com>
2022-06-22 11:07:36 +0200

    V0.0.2 ajout des fonctions psutil de cpu et de memoire

---
+++ CPU/psutilCPU.py
@@ -0,0 +1,63 @@
+import psutil
+import logging
+
+
+def print_cpu_time():
+    """
+    print the cpu time
+    :return: cpu time
+    """
+    return ["CPU time:",psutil.cpu_times()]
+
+
+def print_cpu_percent():
+
+    """
+    print the cpu percent
+    :return: cpu percent
+    """
+    return ["CPU percent:",psutil.cpu_percent(interval=1)]
+
+def print_cpu_count():
+    """
+    print the cpu percent
+    :return: cpu percent
+    """
+    return ["CPU count",psutil.cpu_count()]
+
+def print_cpu_stat():
+    """
+    print the cpu stats
+    :return: cpu stats
+    """
+    return ["CPU stats",psutil.cpu_stats()]
+
+def print_cpu_freq():
+    """
+    return cpu freq
+    :return: cpu freq
+    """
+    return ["cpu freq",psutil.cpu_freq()]
+
+def print_cpu_avg():
+    """
+    return cpu avg
+    :return: cpu avg
+    """
+    return ["cpu avg",psutil.getloadavg()]
+
+
+def get_all_info_cpu():
+    allinfo = []
+
+    allinfo.append(print_cpu_time())
+    allinfo.append(print_cpu_percent())
+    allinfo.append(print_cpu_count())
+    allinfo.append(print_cpu_stat())
+    allinfo.append(print_cpu_freq())
+    allinfo.append(print_cpu_avg())
+
+    return allinfo
+
+
+
---
+++ Memory/psutilMemory.py
@@ -0,0 +1,28 @@
+import psutil
+
+def print_virtual_memory():
+    """
+    print virtual memory
+    :return: virtual memory
+    """
+    return ["virtual memory",psutil.virtual_memory()]
+
+def print_swap_memory():
+    """
+        print swap memory
+        :return: swap memory
+        """
+    return ["swap memory",psutil.swap_memory()]
+
+def print_all_info_memory():
+    """
+    :return: all info memory
+    """
+    allinfo = []
+
+    allinfo.append(print_virtual_memory())
+    allinfo.append(print_swap_memory())
+
+    return allinfo
+
+print(print_all_info_memory())
