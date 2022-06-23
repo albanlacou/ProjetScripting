d7cb9f773d9b9b34d17d37ae39592475eecb9285
Loic-Gouriou <loicgouriou6@gmail.com>
2022-06-23 20:18:14 +0200

    V0.1.3 mise en plzce du test pour le script de mise en bdd

--- Data.py
+++ Data.py
@@ -1,23 +1,25 @@
 import influxdb_client, os, time
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
+import CPU.psutilCPU as CPU
 
 token = os.environ.get("INFLUXDB_TOKEN")
-org = "belli.kieran@gmail.com"
-url = "https://us-east-1-1.aws.cloud2.influxdata.com"
+org = "loic.gouriou@edu.itescia.fr"
+url = "http://192.168.1.40:8086"
 
 client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 
 
-bucket = "Logs"
-
 write_api = client.write_api(write_options=SYNCHRONOUS)
-
-for value in range(5):
-    point = (
-        Point("measurement1")
-        .tag("tagname1", "tagvalue1")
-        .field("field1", value)
-    )
-    write_api.write(bucket=bucket, org="belli.kieran@gmail.com", record=point)
-    time.sleep(1)  # separate points by 1 second
+"""
+return rien 
+ajoute les valeurs a notre measurement
+"""
+def write_db(donne):
+        point = (
+            Point("composant")
+            .tag("type", "value")
+            .field(donne[0], donne[1])
+        )
+        write_api.write(bucket=bucket, url='http://192.168.1.40:8086', record=point)
+        time.sleep(1)  # separate points by 1 second
